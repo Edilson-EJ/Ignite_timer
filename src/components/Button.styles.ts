8748b945c5bf3dd5f405fe5e4e2fde1e3e08753b
Edilson-EJ <edilsonjunior624@gmail.com>
2022-11-28 20:33:19 -0300

    ts

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,33 +1,35 @@
-import styled, { css} from "styled-components";
+import styled, { css } from 'styled-components'
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success' | 'neutral';
+export type ButtonVariant =
+  | 'primary'
+  | 'secondary'
+  | 'danger'
+  | 'success'
+  | 'neutral'
 
-interface ButtonContainerProps{
-    variant: ButtonVariant;
+interface ButtonContainerProps {
+  variant: ButtonVariant
 }
 
-const ButtonVariants ={
-    primary: 'purple',
-    secondary: 'orange',
-    danger: 'red',
-    success: 'green'
+const ButtonVariants = {
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-    width: 100px;
-    height: 40px;
-    border-radius: 4px;
-    border: 0;
-    margin: 8px;
+  width: 100px;
+  height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-    background-color: ${props => props.theme.primary};
+  background-color: ${(props) => props.theme['green-500']};
 
-/*
-    ${props => {      
-        return css`
-        
-        `
-    }}
+  /*
+    ${(props) => {
+    return css``
+  }}
 */
-
 `
